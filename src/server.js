579222cec46fa4e1156d9dd39743cc4671ebf854
SungwanYoon <SungwanYoon@users.noreply.github.com>
2020-02-04 18:58:07 +0900

    16.02.04

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { join } from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+const app = express();
+app.set("view engine", "pug");
+app.set("views", join(__dirname, "views"));
+app.get("/", (req, res) => res.render("home"));
+
+const handleListening = () =>
+  console.log(`✔ Server running: http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
